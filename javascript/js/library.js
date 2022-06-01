786fd2d4ae6203415ddf51ba0be053134f071c66
Zigmantas <zigmantas@mrorangetech.se>
2022-06-01 13:30:15 +0300

    5 paskaita

---
+++ javascript/js/library.js
@@ -0,0 +1,12 @@
+function printUserName(){
+    // paprašyti įvesti vartotojo vardą ir išvesti jį į konsolę
+    let userName = prompt("Įveskite vartotojo vardą");
+
+    console.log(userName);
+}
+
+function getUserName(){
+    let userName = prompt("Įveskite vartotojo vardą");
+
+    return userName;
+}
