ac250607ec53a0570e0a01f055b6ebf254145e80
Zigmantas <zigmantas@mrorangetech.se>
2022-06-02 22:22:57 +0300

    6 paskaita

--- javascript/js/library.js
+++ javascript/js/library.js
@@ -10,3 +10,45 @@ function getUserName(){
 
     return userName;
 }
+
+// function doesUserExist(username){
+//     let usernames = ["jonas123", "onute4", "petras55"];
+
+//     for (let index = 0; index < usernames.length; index++) {
+//         const element = usernames[index];
+
+//         console.log(index);
+
+//         if(element == username){
+//             return true;
+//         }
+//     }
+
+//     return false;
+// }
+
+function doesUserExist(username){
+    return ["jonas123", "onute4", "petras55"].includes(username);
+}
+
+function printUserValidationMessage(user){
+    if (doesUserExist(user)) {
+        document.querySelector("p").innerText = "Sveiki prisijungę";
+        document.querySelector("p").style.color = "green";
+    } else {
+        document.querySelector("p").innerText = "Jūs nerastas sistemoje";
+        document.querySelector("p").style.color = "red";
+    }
+}
+
+function calculateArrayNumbersAverage(array){
+
+    let sum = 0;
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        
+        sum += element;
+    }
+
+    return sum/array.length;
+}
---
+++ javascript/js/studentService.js
@@ -0,0 +1,21 @@
+// funkcija grazina objektu masyva (studentu masyva)
+function getStudents(){
+    let students = [
+        {
+            name: "Jonas",
+            surname: "Jonaitis",
+            age: 25,
+            photo: "images/jonas.jfif",
+            grades: [10, 8, 7, 9, 4]
+        },
+        {
+            name: "Onutė",
+            surname: "Pakalnutė",
+            age: 25,
+            photo: "images/onute.jpg",
+            grades: [9, 8, 9, 10, 8]
+        }
+    ];
+
+    return students;
+}
